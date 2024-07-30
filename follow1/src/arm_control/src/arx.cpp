0eda16ed43f95c518acb0f0ec7080118d448c6ef
yonglin.huang <yonglin.huang@agilex.ai>
2024-07-30 15:21:08 +0800

    delete vel

--- follow1/src/arm_control/src/arx.cpp
+++ follow1/src/arm_control/src/arx.cpp
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
                                     for(int8_t i = 0; i < 7; ++i)
                                     {
                                     ARX_ARM.ros_control_pos_t[i] = msg->position[i];
-                                    ARX_ARM.ros_control_spd_t[i] = msg->velocity[i];
+                                    // ARX_ARM.ros_control_spd_t[i] = msg->velocity[i];
                                     }
                                 });
     ros::Subscriber sub_pos = node.subscribe<geometry_msgs::PoseStamped>("/master/end_right", 10, 
